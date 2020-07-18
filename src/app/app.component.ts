4f21a177558943191f142f29e9f55bef0a179203
max <maximenorval@gmail.com>
2020-07-18 18:53:09 +0200

    button to turn on every devices writes in the console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-oc';
+  isAuthenticated: boolean = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.isAuthenticated = true;
+    }, 4000);
+  }
+
+  turnOnEverything(): void {
+    console.log('On allume tout !');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { DeviceComponent } from './device/device.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    DeviceComponent,
   ],
   imports: [
     BrowserModule,
