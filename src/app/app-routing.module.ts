0c68c34f2fe70bd7ba3483df50481431ccf67556
max <maximenorval@gmail.com>
2020-07-21 23:36:36 +0200

    [DONE] 404 not found works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { DevicesViewComponent } from './devices-view/devices-view.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { DetailDeviceComponent } from './detail-device/detail-device.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'authentication', pathMatch: 'full' },
   { path: 'authentication', component: AuthenticationComponent },
   { path: 'devices', component: DevicesViewComponent },
   { path: 'devices/:id', component: DetailDeviceComponent },
+  { path: '**', redirectTo: 'not-found' },
+  { path: 'not-found', component: NotFoundComponent },
 ];
 
 @NgModule({
