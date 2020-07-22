3f4c8a25522aee848363af1aa68397b96b68fe39
max <maximenorval@gmail.com>
2020-07-22 07:38:57 +0200

    counter works and displays connection time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  seconds: number;
+
   constructor() {}
+
+  ngOnInit() {
+    const counter = interval(1000);
+
+    counter.subscribe(
+      (value) => {
+        this.seconds = value;
+      },
+      (error) => {
+        console.log('Oups, il y a eu une erreur : ' + error);
+      },
+      () => {
+        console.log('Observable complet !');
+      }
+    );
+  }
 }
